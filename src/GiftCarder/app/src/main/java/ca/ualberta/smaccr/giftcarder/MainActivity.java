06f96bc20818e3de4f12e9e845dcaba7476a3da8
cbli <cbli@ualberta.ca>
2015-10-23 01:22:45 -0600

    Added login/registration UI

--- src/GiftCarder/app/src/main/java/ca/ualberta/smaccr/giftcarder/MainActivity.java
+++ src/GiftCarder/app/src/main/java/ca/ualberta/smaccr/giftcarder/MainActivity.java
@@ -5,8 +5,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
+    public final static String EXTRA_USERNAME= "ca.ualberta.smaccr.giftcarder.USERNAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,23 +24,8 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void notiButtonClick(MenuItem v){
-        Intent intent = new Intent(this, NotiFullActivity.class);
+    public void registerNewUser(View view){
+        Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     }
 }
