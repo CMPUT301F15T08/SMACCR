b6824919c3cc9f84ed421e6f81ce924dbb028d71
splant10 <splant.10@gmail.com>
2015-11-25 14:50:35 -0700

    small format changes

--- src/GiftCarder/app/src/main/java/ca/ualberta/smaccr/giftcarder/InventoryActivity.java
+++ src/GiftCarder/app/src/main/java/ca/ualberta/smaccr/giftcarder/InventoryActivity.java
@@ -59,16 +59,15 @@ public class InventoryActivity extends Activity {
         inventorylistID.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //Toast.makeText(getApplicationContext(), Integer.toString(position), Toast.LENGTH_SHORT).show();
 
                 // Switch to item activity and send inventory and position of gift card to change
                 Intent intent = new Intent(InventoryActivity.this, ItemActivity.class);
-                //intent.putExtra("GiftCard", inv.getInvList().get(position));
+                // intent.putExtra("GiftCard", inv.getInvList().get(position));
                 intent.putExtra("position", position);
                 intent.putExtra("inventory", inv);
-               // intent.putExtra("ownerInventory", ownerInv);
+                // intent.putExtra("ownerInventory", ownerInv);
                 intent.putExtra(EXTRA_STATE, BROWSER_ITEM_STATE); // view item
-                //startActivity(intent);
+                // startActivity(intent);
                 startActivityForResult(intent, 1);
             }
         });
